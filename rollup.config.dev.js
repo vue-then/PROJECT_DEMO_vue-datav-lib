e22533195e585a5280d31b9baa068968952eac4e
chenzihao <278199196@qq.com>
2020-06-28 22:23:11 +0800

    chapter 11-2：rollup 切换模块化标准+多模块构建

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -1,13 +1,21 @@
 const path = require('path')
 
 const inputPath = path.resolve(__dirname, './src/index.js')
-const outputPath = path.resolve(__dirname, './dist/imooc.davav.js')
+const outputUmdPath = path.resolve(__dirname, './dist/imooc.davav.js')
+const outputEsPath = path.resolve(__dirname, './dist/imooc.davav.es.js')
 
 module.exports = {
   input: inputPath,
-  output: {
-    file: outputPath,
-    name: 'imoocDataV', // 模块名称
-    format: 'umd' // 输出的模块协议
-  }
+  output: [
+    {
+      file: outputUmdPath,
+      name: 'imoocDataV', // 模块名称
+      format: 'umd' // 输出的模块协议
+    },
+    {
+      file: outputEsPath,
+      name: 'imoocDataV', // 模块名称P
+      format: 'es' // 输出的es6模块协议
+    }
+  ]
 }
