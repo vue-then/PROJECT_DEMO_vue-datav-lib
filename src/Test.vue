8aefc4a42c5ae31ef324b42e2ed04411d9965c2d
chenzihao <278199196@qq.com>
2020-06-29 22:53:22 +0800

    chapter 11-8：在html中使用自定义组件

--- src/Test.vue
+++ src/Test.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="test">
-    {{msg}}
+    {{msg}}--{{count}}--{{doubleCount}}
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import { computed, ref } from 'vue'
+
 export default {
   name: 'TestComponent',
 
   setup() {
     const msg = '测试'
+    const count = ref(1)
+    const doubleCount = computed(() => count.value * 2)
 
     return {
-      msg
+      msg,
+      count,
+      doubleCount
     }
   }
 }
