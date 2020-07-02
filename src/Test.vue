a47958c5822b3bc21d910fdada4a8e315f5fae9a
chenzihao <278199196@qq.com>
2020-07-02 22:56:32 +0800

    chapter 12-2：svg viewport 和viewBox 用法讲解

--- src/Test.vue
+++ src/Test.vue
@@ -1,7 +1,27 @@
 <template>
-  <div class="test">
-    {{msg}}--{{count}}--{{doubleCount}}
-  </div>
+  <!-- 如果不写viewBox，则默认viewBox的宽高和viewport一致 -->
+  <svg
+    width="100%"
+    height="100%"
+    viewBox="0 0 100 100"
+  >
+    <line
+      x1="0"
+      y1="50"
+      x2="100"
+      y2="50"
+      stroke-width="8"
+      stroke="currentColor"
+    ></line>
+    <line
+      x1="50"
+      y1="0"
+      x2="50"
+      y2="100"
+      stroke-width="8"
+      stroke="currentColor"
+    ></line>
+  </svg>
 </template>
 
 <script type="text/ecmascript-6">
@@ -24,7 +44,4 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-  .test {
-    color: red;
-  }
 </style>
