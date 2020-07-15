f3916223f3a06e86dd3340b53e5020a3eeb8c8bc
chenzihao <278199196@qq.com>
2020-07-15 22:10:21 +0800

    chapter 14-1：loading组件样式开发

---
+++ src/components/ImoocLoading/ImoocLoading.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="imooc-loading">
+    <svg width="200" height="200" viewBox="0 0 50 50">
+      <!-- 
+        stroke-dasharray= 2 * pie * r/4 = 2*3.1415926*22/4
+        stroke-linecap="round"设置两端圆角
+      -->
+      <circle cx="25" cy="25" r="22" fill="none" stroke-width="3" stroke="#3be6cb" stroke-dasharray="34" stroke-linecap="round"></circle>
+      <circle cx="25" cy="25" r="12" fill="none" stroke-width="3" stroke="#02bcfe" stroke-dasharray="19" stroke-linecap="round"></circle>
+    </svg> 
+  </div>
+</template>
+
+<script type="text/ecmascript-6">
+export default {
+  name: 'ImoocLoading',
+
+  data() {
+    return {
+      
+    }
+  },
+
+  methods: {
+      
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  
+</style>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 // es6只有export可以触发tree shaking机制。commonjs只有exports可以触发tree shaking机制
-import Test from './Test.vue'
+import Test from './components/Test/index'
 import Icon from './components/Icon/index'
 import SvgAnimation from './components/SvgAnimation/index'
+import ImoocLoading from './components/ImoocLoading/index'
 
 export default function(Vue) {
-  Vue.component(Test.name, Test)
+  Vue.use(Test)
   Vue.use(Icon)
   Vue.use(SvgAnimation)
+  Vue.use(ImoocLoading)
 }
