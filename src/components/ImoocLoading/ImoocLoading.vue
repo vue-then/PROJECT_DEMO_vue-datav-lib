36ee62e69a0032a13400115fdbbd288fd5f31322
chenzihao <278199196@qq.com>
2020-07-15 22:48:13 +0800

    chapter 14-3：loading组件扩展能力开发

--- src/components/ImoocLoading/ImoocLoading.vue
+++ src/components/ImoocLoading/ImoocLoading.vue
@@ -1,9 +1,10 @@
 <template>
   <div class="imooc-loading">
     <svg
-      width="200"
-      height="200"
+      :width="width"
+      :height="height"
       viewBox="0 0 50 50"
+      preserveAspectRatio="xMidYMid meet"
     >
       <!-- 
         stroke-dasharray= 2 * pie * r/4 = 2*3.1415926*22/4
@@ -17,7 +18,7 @@
         r="22"
         fill="none"
         stroke-width="3"
-        stroke="#3be6cb"
+        :stroke="outsideColor"
         stroke-dasharray="34"
         stroke-linecap="round"
       >
@@ -33,13 +34,13 @@
           attributeName="transform"
           type="rotate"
           values="0 25 25;360 25 25"
-          dur="2s"
+          :dur="dur"
           repeatCount="indefinite"
         ></animateTransform>
         <animate
           attributeName="stroke"
-          values="#3be6cb;#02bcfe;#3be6cb"
-          dur="2s"
+          :values="outsideColorAnimation"
+          :dur="dur"
           repeatCount="indefinite"
         ></animate>
       </circle>
@@ -49,7 +50,7 @@
         r="12"
         fill="none"
         stroke-width="3"
-        stroke="#02bcfe"
+        :stroke="insideColor"
         stroke-dasharray="19"
         stroke-linecap="round"
       >
@@ -65,32 +66,68 @@
           attributeName="transform"
           type="rotate"
           values="360 25 25;0 25 25"
-          dur="2s"
+          :dur="dur"
           repeatCount="indefinite"
         ></animateTransform>
         <animate
           attributeName="stroke"
-          values="#02bcfe;#3be6cb;#02bcfe"
-          dur="2s"
+          :values="insideColorAnimation"
+          :dur="dur"
           repeatCount="indefinite"
         ></animate>
       </circle>
     </svg>
+    <div class="imooc-loading-content">
+      <slot></slot>
+    </div>
   </div>
 </template>
 
 <script type="text/ecmascript-6">
+import {computed} from 'vue'
+
 export default {
   name: 'ImoocLoading',
 
-  data() {
-    return {
-
+  props: {
+    width: {
+      type: [Number, String],
+      default: 50
+    },
+    height: {
+      type: [Number, String],
+      default: 50
+    },
+    outsideColor: {
+      type: String,
+      default: '#3be6cb'
+    },
+    insideColor: {
+      type: String,
+      default: '#02bcfe'
+    },
+    duration: {
+      type: [Number, String],
+      default: 2
     }
   },
 
-  methods: {
+  setup(ctx) {
+    const outsideColorAnimation = computed(() => `
+      ${ctx.outsideColor};${ctx.insideColor};${ctx.outsideColor}
+    `)
 
+    const insideColorAnimation = computed(() => `
+      ${ctx.insideColor};${ctx.outsideColor};${ctx.insideColor}
+    `)
+
+    const dur = computed(() => `${ctx.duration}s`)
+
+    return {
+      outsideColorAnimation,
+      insideColorAnimation,
+      dur
+    }
   }
 }
 </script>
