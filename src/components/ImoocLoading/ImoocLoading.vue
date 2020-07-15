7d63b440eeba3195660b099dbbd9aa13213fe56a
chenzihao <278199196@qq.com>
2020-07-15 22:23:16 +0800

    chapter 14-2：loading组件动画开发

--- src/components/ImoocLoading/ImoocLoading.vue
+++ src/components/ImoocLoading/ImoocLoading.vue
@@ -1,13 +1,81 @@
 <template>
   <div class="imooc-loading">
-    <svg width="200" height="200" viewBox="0 0 50 50">
+    <svg
+      width="200"
+      height="200"
+      viewBox="0 0 50 50"
+    >
       <!-- 
         stroke-dasharray= 2 * pie * r/4 = 2*3.1415926*22/4
         stroke-linecap="round"设置两端圆角
+        from的第2个第3个参数可以设置以某个坐标点做变换
+        可以通过values传递多个值
       -->
-      <circle cx="25" cy="25" r="22" fill="none" stroke-width="3" stroke="#3be6cb" stroke-dasharray="34" stroke-linecap="round"></circle>
-      <circle cx="25" cy="25" r="12" fill="none" stroke-width="3" stroke="#02bcfe" stroke-dasharray="19" stroke-linecap="round"></circle>
-    </svg> 
+      <circle
+        cx="25"
+        cy="25"
+        r="22"
+        fill="none"
+        stroke-width="3"
+        stroke="#3be6cb"
+        stroke-dasharray="34"
+        stroke-linecap="round"
+      >
+        <!-- <animateTransform
+          attributeName="transform"
+          type="rotate"
+          from="0 25 25"
+          to="360 25 25"
+          dur="2s"
+          repeatCount="indefinite"
+        ></animateTransform> -->
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          values="0 25 25;360 25 25"
+          dur="2s"
+          repeatCount="indefinite"
+        ></animateTransform>
+        <animate
+          attributeName="stroke"
+          values="#3be6cb;#02bcfe;#3be6cb"
+          dur="2s"
+          repeatCount="indefinite"
+        ></animate>
+      </circle>
+      <circle
+        cx="25"
+        cy="25"
+        r="12"
+        fill="none"
+        stroke-width="3"
+        stroke="#02bcfe"
+        stroke-dasharray="19"
+        stroke-linecap="round"
+      >
+        <!-- <animateTransform
+          attributeName="transform"
+          type="rotate"
+          from="360 25 25"
+          to="0 25 25"
+          dur="2s"
+          repeatCount="indefinite"
+        ></animateTransform> -->
+        <animateTransform
+          attributeName="transform"
+          type="rotate"
+          values="360 25 25;0 25 25"
+          dur="2s"
+          repeatCount="indefinite"
+        ></animateTransform>
+        <animate
+          attributeName="stroke"
+          values="#02bcfe;#3be6cb;#02bcfe"
+          dur="2s"
+          repeatCount="indefinite"
+        ></animate>
+      </circle>
+    </svg>
   </div>
 </template>
 
@@ -17,16 +85,15 @@ export default {
 
   data() {
     return {
-      
+
     }
   },
 
   methods: {
-      
+
   }
 }
 </script>
 
 <style lang="scss" scoped>
-  
 </style>
